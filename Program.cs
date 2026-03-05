09a94a3fef809053ae5589139e9fec32191f850d
Vignesh K <Vignesh.K@Tekosol.net>
2026-03-05 16:54:38 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("CI/CD Version Demo");
+
+        Console.WriteLine($"Machine: {Environment.MachineName}");
+        Console.WriteLine($"Time: {DateTime.Now}");
+
+        Console.WriteLine("Application version: v1.0.0");
+    }
+}
